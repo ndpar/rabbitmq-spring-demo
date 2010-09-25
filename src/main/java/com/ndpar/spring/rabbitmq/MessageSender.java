3d9572cab251a1d38f2dba57a6a3e877056bc418
Andrey Paramonov <git@ndpar.com>
2010-09-25 17:30:04 -0400

    Sending message from JMX console

--- src/main/java/com/ndpar/spring/rabbitmq/MessageSender.java
+++ src/main/java/com/ndpar/spring/rabbitmq/MessageSender.java
@@ -2,13 +2,20 @@ package com.ndpar.spring.rabbitmq;
 
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedOperation;
 
 public class MessageSender {
 
     @Autowired
     private AmqpTemplate template;
 
+    @ManagedOperation
     public void send(String text) {
-        template.convertAndSend("NDPAR.SPRING.JAVA", text);
+        send("NDPAR.SPRING.JAVA", text);
+    }
+
+    @ManagedOperation
+    public void send(String key, String text) {
+        template.convertAndSend(key, text);
     }
 }
