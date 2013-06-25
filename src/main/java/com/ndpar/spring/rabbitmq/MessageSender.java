054dd559ec2469801f681bee1bee3c066d263568
Andrey Paramonov <github@ndpar.com>
2013-06-25 16:24:57 -0400

    Removed JMX annotations

--- src/main/java/com/ndpar/spring/rabbitmq/MessageSender.java
+++ src/main/java/com/ndpar/spring/rabbitmq/MessageSender.java
@@ -4,12 +4,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jmx.export.annotation.ManagedOperation;
-import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Service;
 
 @Service
-@ManagedResource
 public class MessageSender {
 
     protected final Log log = LogFactory.getLog(getClass());
@@ -17,12 +14,10 @@ public class MessageSender {
     @Autowired
     private AmqpTemplate template;
 
-    @ManagedOperation
     public void send(String text) {
         send("amq.fanout", "NDPAR.SPRING.JAVA", text);
     }
 
-    @ManagedOperation
     public void send(String exchange, String key, String text) {
         log.debug(String.format("%s -> %s: %s", key, exchange, text));
         template.convertAndSend(exchange, key, text);
